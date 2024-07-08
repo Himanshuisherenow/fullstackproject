f5468f4cdf46c4f4445752d3949609095badbfbf
Chavda Himanshu H <himanshuisherenow@gmail.com>
2024-07-08 23:15:16 +0530

    all component ui pages added

--- frontendx/elib-dashboard/src/main.tsx
+++ frontendx/elib-dashboard/src/main.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {RouterProvider} from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
 import router from './router.tsx';
 
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </React.StrictMode>
+);
